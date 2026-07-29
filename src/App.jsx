829362bdaa3a0eacf20b8a8d04361be5982cdbbe
haroldcano388-eng <haroldcano388@gmail.com>
2026-07-29 22:10:49 +0800

    mana

--- src/App.jsx
+++ src/App.jsx
@@ -6,28 +6,79 @@ function App(){
 const [username, setUsername] = useState(""); 
 const [password, setPassword] = useState("");
 const [email, setEmail] = useState("");
+const [page, setPage] = useState("login");
 return(
-<>
-<h1>LEARN LOOP </h1>
-<label>Name</label><br></br>
-<input 
-type="text"
-value={username}
-placeholder="Enter user yah" required
-onChange={(e) => setUsername(e.target.value)
-}
-/>
-<div>
-  <input type="password"
-   placeholder="mao guro"
-   value={password}
-   onChange={(e) => setPassword(e.target.value)
-   }
-   />
-</div>
+  <>
+ 
+ {page === "login" && (
+
+
+  <div id="container">
+
+    <title>Login Form</title>
+
+    <h1>LEARN LOOP</h1>
+     <h3>LOG IN</h3>
+    <br></br>
+  
+<input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+ <br></br>
+<input type="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+ <br></br>
+<button onClick={() => setPage("ADMINHOME")}>LOGIN</button>
+
+ <br></br>
+  <br></br>
+<button onClick={() => setPage("register")}>REGISTER</button>
+
+  </div>
+ )}
+
+ 
+
+ 
+ {page === "register" && (
+
+<div id="container">
+
+    <title>Register Form</title>
+
+    <h1>LEARN LOOP</h1>
+     <h3>REGISTER</h3>
+    <br></br>
+  <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+ <br></br>
+<input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+ <br></br>
+<input type="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+ <br></br>
+
+ <br></br>
+  <br></br>
+<button onClick={() => setPage("login")}>REGISTER</button>
+  </div>
+ )}
+
+
+
+  
+
+  
+{page === "ADMINHOME" && (
+
+<div id="homecontainer">
+<h1>WELCOME TO ADMIN HOME PAGE</h1>
+
+
+
+<button onClick={() => setPage("login")}>LOG OUT</button>
+ </div>
+ )}
+
+ 
+
+
 </>
-
-
 )
 }
 export default App

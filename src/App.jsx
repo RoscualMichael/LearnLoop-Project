b14d49a0fb34b179cd2bf75bfb1a6c5ef4f1b7f7
Michael <roscualmichael@gmail.com>
2026-07-29 15:48:36 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+
+import { useState } from "react";
+import "./App.css";
+
+function App(){
+const [username, setUsername] = useState(""); 
+const [password, setPassword] = useState("");
+const [email, setEmail] = useState("");
+return(
+<>
+<h1>Mic test 1 2 1 2 </h1>
+<label>Name</label><br></br>
+<input 
+type="text"
+value={username}
+placeholder="Enter user yah" required
+onChange={(e) => setUsername(e.target.value)
+}
+/>
+<div>
+  <input type="password"
+   placeholder="mao guro"
+   value={password}
+   onChange={(e) => setPassword(e.target.value)
+   }
+   />
+</div>
+</>
+
+
+)
+}
+export default App

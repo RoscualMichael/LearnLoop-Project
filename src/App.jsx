a37d1d5f2da94751b26a4e15121ec22b093deb54
haroldcano388-eng <haroldcano388@gmail.com>
2026-07-30 00:37:34 +0800

    homepage design

--- src/App.jsx
+++ src/App.jsx
@@ -62,18 +62,39 @@ return(
 
 
   
-
-  
 {page === "ADMINHOME" && (
+  <div id="homecontainer">
+    <h1>WELCOME TO ADMIN HOME PAGE</h1>
 
-<div id="homecontainer">
-<h1>WELCOME TO ADMIN HOME PAGE</h1>
+    <div className="cards">
+      <div className="card">
+        <h2>5</h2>
+        <p>Users</p>
+      </div>
 
+      <div className="card">
+        <h2>10</h2>
+        <p>Listings</p>
+      </div>
 
+      <div className="card">
+        <h2>3</h2>
+        <p>Reports</p>
+      </div>
+    </div>
 
-<button onClick={() => setPage("login")}>LOG OUT</button>
- </div>
- )}
+    <div className="buttons">
+      <button>Add User</button>
+      <button>View Listings</button>
+      <button>Reports</button>
+    </div>
+
+    <button onClick={() => setPage("login")}>
+      LOG OUT
+    </button>
+  </div>
+)}
+  
 
  
 
